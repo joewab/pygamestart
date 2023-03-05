7ea5670e1919b94f895f39bcf018bb453b6a82dc
Joe AB <joe.anthonybrown@gmail.com>
2023-03-05 16:14:32 -0600

    basic game complete

--- index.py
+++ index.py
@@ -7,6 +7,7 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Traveler')
 clock = pygame.time.Clock()
 pixel_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+game_active = True
 
 sky_surf = pygame.image.load('graphics/Sky.png').convert()
 ground_surf = pygame.image.load('graphics/ground.png').convert()
@@ -29,32 +30,45 @@ while True:
             pygame.quit()
             exit()
         
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos):
-                if player_rect.bottom >= 300:
-                    player_gravity = -20
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos):
+                    if player_rect.bottom >= 300:
+                        player_gravity = -20
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if player_rect.bottom >= 300:
-                    player_gravity = -20
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if player_rect.bottom >= 300:
+                        player_gravity = -20
 
-    screen.blit(sky_surf,(0,0))
-    screen.blit(ground_surf,(0,300))
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
 
-    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10, 10)
-    screen.blit(score_surf, score_rect)
+    if game_active:
+        screen.blit(sky_surf,(0,0))
+        screen.blit(ground_surf,(0,300))
 
-    #snail
-    screen.blit(snail_surf, snail_rect)
-    if snail_rect.left < -10 : snail_rect.left = 800
-    snail_rect.left -= 4
-    
-    #player
-    player_gravity += 1
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 300: player_rect.bottom = 300
-    screen.blit(player_surf, player_rect)
+        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10, 10)
+        screen.blit(score_surf, score_rect)
+
+        #snail
+        screen.blit(snail_surf, snail_rect)
+        if snail_rect.left < -10 : snail_rect.left = 800
+        snail_rect.left -= 4
+        
+        #player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300: player_rect.bottom = 300
+        screen.blit(player_surf, player_rect)
+
+        # collision
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill('Yellow')
 
     pygame.display.update()
     clock.tick(60)
